45ead3d7227f63d020912fdf665e1fcceccbe5a0
RVJVIJAY <vijay31235l@gmail.com>
2024-10-10 10:07:04 +0530

    add Active link in auth folder

--- src/app/(auth)/(innerlayout)/layout.tsx
+++ src/app/(auth)/(innerlayout)/layout.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
-interface Children{
-    children:React.ReactNode
+"use client";
+import Link from "next/link";
+import React from "react";
+import './style.css'
+import { usePathname } from "next/navigation";
+interface Children {
+  children: React.ReactNode;
 }
-const InnerLayout:React.FC<Children> = ({children}) => {
+const InnerLayout: React.FC<Children> = ({ children }) => {
+  const pathname = usePathname();
+  const links = [
+    {
+      name: "Login",
+      href: "/login",
+    },
+    { name: "Register", href: "/register" },
+  ];
   return (
     <div>
+      {links.map((item, index) => {
+        const isActivelink = pathname.startsWith(item.href)
+        return(
+          <Link
+            className={isActivelink ? "font-bold" : "text-blue-600"}
+            key={index}
+            href={item.href}
+          >
+            {item.name}
+          </Link>
+        );
+      })}
       <h1>Inner Layout</h1>
-      <p>when we grp the folder by using () bracket , we no need use the folder url , we can easily grp the folders </p>
+      <p>
+        when we grp the folder by using () bracket , we no need use the folder
+        url , we can easily grp the folders{" "}
+      </p>
       {children}
     </div>
-  )
-}
+  );
+};
 
-export default InnerLayout
+export default InnerLayout;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,7 +8,7 @@ const Products = () => {
        <h2> <Link href={`/products/1`}>Product 1</Link></h2> 
        <h2> <Link href={`/products/2`}>Product 2</Link></h2> 
        <h2> <Link href={`/products/3`}>Product 3</Link></h2> 
-       <h2> <Link href={`/products/4`}>Product 4</Link></h2> 
+       <h2> <Link href={`/products/4`} replace>Product 4</Link></h2> 
     </div>
   )
 }
